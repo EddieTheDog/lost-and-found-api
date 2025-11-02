4d9f6284af8854c076582b9315c997b1d58b800d
EddieTheDog <1dylanroeder1@gmail.com>
2025-11-02 10:52:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import claimRoutes from './routes/claims.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import claimRoutes from "./routes/claims.js";
 
 dotenv.config();
 const app = express();
@@ -9,8 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/claims', claimRoutes);
+app.use("/api/claims", claimRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
